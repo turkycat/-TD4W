199cac19b2841fa60f93b02a55541654de84dbb2
Jesse Frush <jesse.frush@gmail.com>
2014-05-01 05:05:31 -0500

    finished.

---
+++ src/turkycat/productions/td4w/MainActivity.java
@@ -0,0 +1,74 @@
+package turkycat.productions.td4w;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity
+{
+	private MediaPlayer player;
+	private Animation push;
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_main );
+		
+		push = AnimationUtils.loadAnimation( this, R.anim.push );
+		
+		AssetFileDescriptor afd = null;
+		try
+		{
+			afd = getAssets().openFd( "td4w.mp3" );
+			player = new MediaPlayer();
+			player.setDataSource( afd.getFileDescriptor() );
+		}
+		catch( IOException e )
+		{
+			// do nothing
+		}
+	}
+	
+	
+	/**
+	 * callback method invoked from the framework when the button is pressed.
+	 * 	this is defined in the XML, and is handled by the XML inflator
+	 */
+	public void PlayAudio( View view )
+	{	
+		ImageView button = (ImageView) view.findViewById( R.id.button );
+		button.startAnimation( push );
+		
+		if( player.isPlaying() )
+		{
+			player.stop();
+		}
+
+		try
+		{
+			player.prepare();
+			player.seekTo( 0 );
+		}
+		catch( IllegalStateException | IOException e )
+		{
+			// nothing to do
+			e.printStackTrace();
+		}
+
+		player.start();
+	}
+
+}
